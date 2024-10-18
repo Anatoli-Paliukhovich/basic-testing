fcf67c8c22dd57c46c9af755db19e739e05dfd97
“Anatoli <humanities5050@gmail.com>
2024-10-18 23:19:38 +0300

    feat: implement the first four tasks

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,47 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount, InsufficientFundsError, TransferFailedError } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const acc = getBankAccount(100);
+    expect(acc.getBalance()).toBe(100);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const acc = getBankAccount(100);
+    expect(() => acc.withdraw(200)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const acc = getBankAccount(100);
+    expect(() => acc.transfer(200, getBankAccount(200))).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const acc = getBankAccount(100);
+    expect(() => acc.transfer(100, acc)).toThrowError(TransferFailedError);
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const acc = getBankAccount(100);
+    acc.deposit(200);
+    expect(getBankAccount(100).getBalance()).toBe(100);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const acc = getBankAccount(300);
+    acc.withdraw(200);
+    expect(getBankAccount(300).getBalance()).toBe(300);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const firstAcc = getBankAccount(100);
+    const secondAcc = getBankAccount(100);
+    firstAcc.transfer(50, secondAcc);
+    expect(firstAcc.getBalance()).toBe(50);
+    expect(secondAcc.getBalance()).toBe(150);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
